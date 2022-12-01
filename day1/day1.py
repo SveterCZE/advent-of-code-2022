950685cb7ea265cb09d95630181843a8c42c110c
Petr Šmerkl <46304018+SveterCZE@users.noreply.github.com>
2022-12-01 06:08:39 +0100

    day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,27 @@
+def main():
+    instructions = get_input()
+    part1(instructions)
+    part2(instructions)
 
+def get_input():
+    instructions = []
+    temp_figure = 0
+    f = open("input.txt", "r")
+    for line in f:
+        if len(line) == 1:
+            instructions.append(temp_figure)
+            temp_figure = 0
+        else:
+            temp_figure += int(line.strip())
+    return instructions
+
+def part1(instructions):
+    print(max(instructions))
+    return 0
+
+def part2(instructions):
+    instructions.sort()
+    print(sum(instructions[-3:]))
+    return 0
+
+main()
