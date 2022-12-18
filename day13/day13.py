9be4e7c4a323229797af91f68615da4a2eacb574
Petr Šmerkl <46304018+SveterCZE@users.noreply.github.com>
2022-12-18 19:30:00 +0100

    day 13

--- day13/day13.py
+++ day13/day13.py
@@ -30,21 +30,14 @@ def part1(instructions):
     return 0
 
 def check_pair_valid(checked_pair):
-    while True:
-        left_list = checked_pair[0]
-        right_list = checked_pair[1]
-
-        # Base case /// 
-        # If the left list runs out of items first, the inputs are in the right order. 
-        # If the right list runs out of items first, the inputs are not in the right order.
-
-        if len(left_list) == 0:
-            return True
-        elif len(right_list) == 0:
+    # Iterate over every i-th item in each packet
+    for i in range(len(checked_pair[0])):
+        left_item = checked_pair[0][i]
+        # If the right list is shorter (i.e. you cannot assign a corrsponding value with ), return False
+        try:
+            right_item = checked_pair[1][i]
+        except:
             return False
-
-        left_item = left_list[0]
-        right_item = right_list[0]
 
         # Alternative 1 --- both are integers
         if isinstance(left_item, int) and isinstance(right_item, int):
@@ -53,22 +46,14 @@ def check_pair_valid(checked_pair):
             elif left_item > right_item:
                 return False
             else:
-                new_left_list = left_list[1:]
-                new_right_list = right_list[1:]
-                new_checked_pair = [new_left_list, new_right_list]
-                if (len(new_left_list) != 0 and len(new_right_list) != 0):
-                    pair_valid = check_pair_valid(new_checked_pair)
-                    if pair_valid != None:
-                        return pair_valid
-        
+                pass
+
         # Alternative 2 --- one is list and one is integer
-        elif (isinstance(left_item, list) and isinstance(right_item, int)) or (isinstance(left_item, int) and isinstance(right_item, list)):
+        if (isinstance(left_item, list) and isinstance(right_item, int)) or (isinstance(left_item, int) and isinstance(right_item, list)):
             if isinstance(right_item, int):
-                right_item_value = right_item
-                right_item = [right_item_value]
-            elif isinstance(left_item, int):
-                left_item_value = left_item
-                left_item = [left_item_value]
+                right_item = [right_item]
+            elif isinstance(left_item, int): 
+                left_item = [left_item]
         
         # Alternative 3 --- both are lists
         if isinstance(left_item, list) and isinstance(right_item, list):
@@ -77,10 +62,10 @@ def check_pair_valid(checked_pair):
             if pair_valid != None:
                 return pair_valid
 
-        # Alternative 4 --- Continue checking next item
-        new_left_list = left_list[1:]
-        new_right_list = right_list[1:]
-        checked_pair = [new_left_list, new_right_list]
+    # Return false if ran through all items, but there are still 
+    if isinstance(checked_pair[0], list) and isinstance(checked_pair[1], list):
+        if len(checked_pair[0]) < len(checked_pair[1]):
+            return True
 
 def part2(instructions):
     pass
