c964b1f055a46b81c7f843000ba11dbdc815c31d
Petr Šmerkl <46304018+SveterCZE@users.noreply.github.com>
2023-01-03 22:46:11 +0100

    day 20

--- day20/day20.py
+++ day20/day20.py
@@ -1,6 +1,7 @@
 def main():
     instructions = get_input()
     part1(instructions)
+    instructions = get_input()
     part2(instructions)
 
 def get_input():
@@ -13,31 +14,41 @@ def get_input():
     return instructions
 
 def part1(instructions):
+    iterate_over_items_in_list(instructions)
+    print(calculate_sum_of_relevant_figures(instructions))
+    return 0
+
+def part2(instructions):
+    encrypt_input(instructions)
+    for i in range(10):
+        iterate_over_items_in_list(instructions)
+    print(calculate_sum_of_relevant_figures(instructions))
+    return 0
+
+def encrypt_input(instructions):
+    decryption_key = 811589153
     for i in range(instructions.get_circular_list_length()):
         currently_moved_node = instructions.get_node_by_order(i)
-        if currently_moved_node.get_node_value() > 0:
-            currently_moved_node.move_node_right(currently_moved_node.get_node_value())
-        elif currently_moved_node.get_node_value() < 0:
-            currently_moved_node.move_node_left(currently_moved_node.get_node_value())
-        else:
-            pass
-        # checked_value = instructions.first_inserted_item
-        # new_list = []
-        # for j in range(instructions.get_circular_list_length()):
-        #     new_list.append(checked_value.get_node_value())
-        #     checked_value = checked_value.next_value
-        # print(i,new_list)
+        currently_moved_node.node_value = currently_moved_node.node_value * decryption_key
+
+def calculate_sum_of_relevant_figures(instructions):
     checked_figure = instructions.find_zero_value()
     sum_of_relevant_figures = 0
     for i in range(1,3001):
         checked_figure = checked_figure.next_value
         if i % 1000 == 0:
             sum_of_relevant_figures += checked_figure.get_node_value()
-    print(sum_of_relevant_figures)
+    return sum_of_relevant_figures
 
-
-def part2(instructions):
-    pass
+def iterate_over_items_in_list(instructions):
+    for i in range(instructions.get_circular_list_length()):
+        currently_moved_node = instructions.get_node_by_order(i)
+        if currently_moved_node.get_node_value() > 0:
+            currently_moved_node.move_node_right(currently_moved_node.get_node_value(), instructions.get_circular_list_length())
+        elif currently_moved_node.get_node_value() < 0:
+            currently_moved_node.move_node_left(currently_moved_node.get_node_value(), instructions.get_circular_list_length())
+        else:
+            pass
 
 class CircularList:
     def __init__(self):
@@ -80,9 +91,8 @@ class CircularNode:
         self.previous_value = previous_value
         self.next_value = None
     
-    def move_node_right(self, move_count):
-        # TODO --- modulo
-        for i in range(move_count):
+    def move_node_right(self, move_count, list_len):
+        for i in range(move_count % (list_len - 1)):
             left_node = self.previous_value
             node_moving_left = self.next_value
             right_node = self.next_value.next_value
@@ -96,8 +106,8 @@ class CircularNode:
             node_moving_left.next_value = self
             self.previous_value = node_moving_left
 
-    def move_node_left(self, move_count):
-        for i in range(abs(move_count)):
+    def move_node_left(self, move_count, list_len):
+        for i in range(abs(move_count) % (list_len - 1)):
             left_node = self.previous_value.previous_value
             node_moving_right = self.previous_value
             right_node = self.next_value
